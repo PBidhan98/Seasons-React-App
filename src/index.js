066ff64c35c23881fc8999b88d069b2d8bae6692
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-14 23:03:47 +0530

    loading spinner & default props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -18,13 +19,13 @@ class App extends React.Component {
   render() {
 
       if(this.state.errorMessage && !this.state.lat) {
-        return   <div>Error: {this.state.errorMessage}</div>;
+        return <div>Error: {this.state.errorMessage}</div>;
       }
       if(!this.state.errorMessage && this.state.lat) {
-        return   <SeasonDisplay lat={this.state.lat}/>
+        return <SeasonDisplay lat={this.state.lat}/>
       }
 
-      return <div>Loading!</div>;
+      return <Spinner message="Please accept location request"/>;
   }
 }
 
