522d6492ae793a8289cd291c04a722703b75563d
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-14 20:05:20 +0530

    Initialising state and updating

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+//this is the very first function i.e. going to be called any time the instance of this class is created
+  constructor(props){
+    super(props);
+    //reference to the parents constructor function
+    this.state = {lat: null};
+    //initialising the state
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      //first argument is a callback function
+      (position) => {
+        //we called setState!!!!!!!!!
+        this.setState({ lat: position.coords.latitude});
+
+        //we did not!!
+        //this.state.lat = position.coords.latitude;
+      },
+      err => console.log(err)
     );
-    return <div>Latitude:</div>;
+  }
+  //React says we have to define render!!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
