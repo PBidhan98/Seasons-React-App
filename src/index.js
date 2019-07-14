30abfbb3a25cb90d051757fe059f30e96d276c69
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-14 23:09:03 +0530

    Avoiding conditionals in render

--- src/index.js
+++ src/index.js
@@ -15,17 +15,25 @@ class App extends React.Component {
     );
   }
 
+  renderContent(){
+    if(this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if(!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <Spinner message="Please accept location request"/>;
+  }
+
+
   //React says we have to define render!!!
   render() {
-
-      if(this.state.errorMessage && !this.state.lat) {
-        return <div>Error: {this.state.errorMessage}</div>;
-      }
-      if(!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat}/>
-      }
-
-      return <Spinner message="Please accept location request"/>;
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
