89be7a35b50bddc9241f9aae92236e32cfa63c4f
Preeti Bidhan <preetibidhan98@gmail.com>
2019-07-14 20:20:31 +0530

    Conditional rendering content

--- src/index.js
+++ src/index.js
@@ -25,13 +25,15 @@ class App extends React.Component {
   }
   //React says we have to define render!!!
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+
+      if(this.state.errorMessage && !this.state.lat) {
+        return   <div>Error: {this.state.errorMessage}</div>;
+      }
+      if(!this.state.errorMessage && this.state.lat) {
+        return   <div>Latitude: {this.state.lat}</div>;
+      }
+
+      return <div>Loading!</div>;
   }
 }
 
